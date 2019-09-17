a0ff3974a158932f529c8a5af34f6510cc461764
fgrimau <florent.grimau@ulb.be>
2019-09-17 21:29:13 +0200

    Upgrade administration

--- administration/urls.py
+++ administration/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 import administration.views
 
 urlpatterns = [
-    url(r'^$',         administration.views.adminPage),
-    url(r'modify',     administration.views.modifyUser),
+    url(r'^$',                              administration.views.adminPage),
+    url(r'users/(?P<string>[\w\-]+)/',      administration.views.userAdminView),
+    url(r'users/',                          administration.views.userAdminView),
+    url(r'modify',                          administration.views.modifyUser),
     url(r'reactivate/(?P<string>[\w\-]+)/', administration.views.reactivate),
 ]
