aa42316885974f6b22b048c9b196b742b9b9f7e1
fgrimau <florent.grimau@ulb.be>
2019-09-17 20:21:27 +0200

    made dpep8 compliant + bug fix

--- administration/views.py
+++ administration/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.contrib.admin.views.decorators import staff_member_required
 import datetime
+
 
 @staff_member_required
 def adminPage(request):
     users = User.objects.all()
     firstUser = users[0].username
 
-    level_ = {'5' : 'Langue maternelle',
-              '4' : 'Très bon',
-              '3' : 'Bon',
-              '2' : 'Notions de base',
-              '1' : 'Aucun'}
+    level_ = {'5': 'Langue maternelle',
+              '4': 'Très bon',
+              '3': 'Bon',
+              '2': 'Notions de base',
+              '1': 'Aucun'}
 
-    lang_ = {'French'  : 'Francais',
-             'Dutch'   : 'Néerlandais',
-             'English' : 'Anglais'}
+    lang_ = {'French': 'Francais',
+             'Dutch': 'Néerlandais',
+             'English': 'Anglais'}
 
-    vars_ = {'a_users' : users, 'a_firstUserUsername' : firstUser, "i_langLevel" : level_, "i_lang" : lang_}
+    vars_ = {
+        'a_users': users,
+        'a_firstUserUsername': firstUser,
+        "i_langLevel": level_,
+        "i_lang": lang_}
 
     return render(request, 'admin/admin.html', vars_)
+
 
 @staff_member_required
 def reactivate(request, string=""):
@@ -38,7 +44,19 @@ def serializeDate(date_):
     """
         returns the date from "day month year" to "ddmmyyyy"
     """
-    months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"]
+    months = [
+        "janvier",
+        "février",
+        "mars",
+        "avril",
+        "mai",
+        "juin",
+        "juillet",
+        "aout",
+        "septembre",
+        "octobre",
+        "novembre",
+        "décembre"]
     date_ = date_.split()
     date_str = date_[0]
     if len(str(date_[0])) == 1:
@@ -53,25 +71,29 @@ def serializeDate(date_):
 
     return datetime.datetime.strptime(date_str, "%d%m%Y")
 
+
 @staff_member_required
 def modifyUser(request):
-    if request.method == "POST": #Check if the way the user accessed this url is correct
+    # Check if the way the user accessed this url is correct
+    if request.method == "POST":
         try:
             form = request.POST
-            if "modify" in form.keys(): #If the admin wants to modify the user
+            # If the admin wants to modify the user
+            if "modify" in form.keys():
                 usr = User.objects.get(username=form["username"])
                 usr.first_name = form["firstName"]
-                usr.last_name  = form["lastName"]
-                usr.email      = form["mail"]
-                usr.save() #Saves basic user model
+                usr.last_name = form["lastName"]
+                usr.email = form["mail"]
+                usr.save()  # Saves basic user model
+                profile = usr.profile
 
-                try: #Checks if the user as a profile extension
-                    type = usr.profile.account_type #Get account type to try
+                try:  # Checks if the user as a profile extension
+                    type = profile.account_type  # Get account type to try
 
-                    #Modifications that apply for ll type of account
-                    usr.profile.phone_number = form["phone_number"]
-                    usr.profile.address      = form["address"]
-                    usr.profile.birthDate    = serializeDate(form["birthDate"])
+                    # Modifications that apply for ll type of account
+                    profile.phone_number = form["phone_number"]
+                    profile.address = form["address"]
+                    profile.birthDate = serializeDate(form["birthDate"])
 
                     for course in ["Maths", "Chimie", "Physique", "Francais"]:
                         if course+"_Course" in form.keys():
@@ -79,55 +101,70 @@ def modifyUser(request):
                         else:
                             exec("usr.profile." + course + "_course = False")
 
-                    if type == "Etudiant": #Modifications that only apply to student type account
-                        usr.profile.wanted_schedule = ""
-                        for day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
+                    # Modifications that only apply to student type account
+                    if type == "Etudiant":
+                        profile.wanted_schedule = ""
+                        days = [
+                            'Monday',
+                            'Tuesday',
+                            'Wednesday',
+                            'Thursday',
+                            'Friday',
+                            'Saturday',
+                            'Sunday']
+
+                        for day in days:
                             id = "course " + day
                             try:
                                 if form[id] == 'on':
-                                    usr.profile.wanted_schedule += "1/"
-                                    usr.profile.wanted_schedule += form[day+"Start"] + "/"
-                                    usr.profile.wanted_schedule += form[day+"End"]   + "."
-                            except:
+                                    profile.wanted_schedule += "1/"
+                                    profile.wanted_schedule += form[day+"Start"] + "/"
+                                    profile.wanted_schedule += form[day+"End"] + "."
+                            except KeyError:
                                 usr.profile.wanted_schedule += "0/0/0."
 
-                        usr.profile.NeedsVisit = False
+                        profile.NeedsVisit = False
                         if form["Visit"] != "NoVisit":
-                            usr.profile.NeedsVisit = True
+                            profile.NeedsVisit = True
 
-                        usr.profile.comments = form["comments"]
+                        profile.comments = form["comments"]
 
-                        usr.profile.tutor_firstName = form["tutorFirstName"]
-                        usr.profile.tutor_name      = form["tutorName"]
-                    elif type == "Coach": #Modifications that only apply to coach type account
-                        usr.profile.school             = form["school"]
-                        usr.profile.IBAN               = form["IBAN"]
-                        usr.profile.nationalRegisterID = form["natRegID"]
+                        profile.tutor_firstName = form["tutorFirstName"]
+                        profile.tutor_name = form["tutorName"]
+                    # Modifications that only apply to coach type account
+                    elif type == "Coach":
+                        profile.school = form["school"]
+                        profile.IBAN = form["IBAN"]
+                        profile.nationalRegisterID = form["natRegID"]
 
-                        usr.profil.French_level  = form["Frenchlevel"]
-                        usr.profil.English_level = form["Englishlevel"]
-                        usr.profil.Dutch_level   = form["Dutchlevel"]
+                        profile.French_level = form["Frenchlevel"]
+                        profile.English_level = form["Englishlevel"]
+                        profile.Dutch_level = form["Dutchlevel"]
 
-                    usr.profile.save() #Saving the profile extension model
+                    profile.save()  # Saving the profile extension model
 
-                except Exception as e: #If the user as no profile extension
-                    print("Error :", e) #Print the error in case it's not because the user has no profile
+                except Exception as e:  # If the user as no profile extension
+                    # Print the error in case it's not because the user
+                    # has no profile
+                    print("Error :", e)
 
-                return HttpResponseRedirect("/administration") #Redirect to administration
+                # Redirect to administration
+                return HttpResponseRedirect("/administration")
             elif "reactivate" in form.keys():
                 print("user is reactive")
-                return HttpResponseRedirect("/administration/reactivate/"+form["username"])
-            else: #If the admin wants to 'delete' the user
+                return HttpResponseRedirect(
+                    "/administration/reactivate/"+form["username"])
+            else:  # If the admin wants to 'delete' the user
                 usr = User.objects.get(username=form["username"])
                 if usr.is_active:
-                    usr.is_active = False #Deactivate the user's account
+                    usr.is_active = False  # Deactivate the user's account
                     usr.save()
                 else:
                     print("user deleted")
-                    usr.delete() #Completely deletes it
+                    usr.delete()  # Completely deletes it
                 return HttpResponseRedirect("/administration")
-        except Exception as e: #In case an error occurs
-            print(e) #Print it
+        except Exception as e:  # In case an error occurs
+            print(e)  # Print it
             return HttpResponseRedirect('/administration')
     else:
         return HttpResponseRedirect('/administration')
