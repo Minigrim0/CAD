34155f82b4403d037abd1a654a3b6b252ed916d8
fgrimau <florent.grimau@ulb.be>
2019-09-17 22:06:16 +0200

    Added explanations

--- administration/urls.py
+++ administration/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$',                              administration.views.adminPage),
     url(r'users/(?P<string>[\w\-]+)/',      administration.views.userAdminView),
     url(r'users/',                          administration.views.userAdminView),
-    url(r'modify',                          administration.views.modifyUser),
+    url(r'modify/',                          administration.views.modifyUser),
     url(r'reactivate/(?P<string>[\w\-]+)/', administration.views.reactivate),
 ]
