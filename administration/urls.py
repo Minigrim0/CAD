8ff08386280b2196a775b24beca2321616fc7f08
fgrimau <florent.grimau@ulb.be>
2020-02-22 13:33:19 +0100

    Named the url routing + modified the templates

--- administration/urls.py
+++ administration/urls.py
@@ -4,12 +4,12 @@ import administration.views
 urlpatterns = [
     url(r'^$', administration.views.adminPage),
     url(r'users/(?P<string>[\w\-]+)/', administration.views.userAdminView),
-    url(r'users/', administration.views.userAdminView),
+    url(r'users/', administration.views.userAdminView, name="user_admin"),
     url(r'articles/modify/', administration.views.articleAdminModify),
-    url(r'articles/', administration.views.articleAdminView),
+    url(r'articles/', administration.views.articleAdminView, name="articles_admin"),
     url(r'mails/modify/', administration.views.mailAdminModify),
     url(r'mails/create/', administration.views.mailAdminCreate),
-    url(r'mails/', administration.views.mailAdminView),
+    url(r'mails/', administration.views.mailAdminView, name="mails_admin"),
     url(r'modify/', administration.views.modifyUser),
     url(r'reactivate/(?P<string>[\w\-]+)/', administration.views.reactivate),
 ]
