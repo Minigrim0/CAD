dbaef4f4df6f0019e95b54d60e723443a8560bc4
Minigrim0 <florent.grimau@outlook.com>
2020-07-27 19:31:06 +0200

    [Upd] docker

--- env.prod.sh
+++ env.prod.sh
@@ -1,10 +1,9 @@
-export SECRET_KEY=***REMOVED***
-export DEBUG=0
-export SQL_DATABASE=***REMOVED***
-export DJANGO_ALLOWED_HOSTS='cadcoursadomicile.com 127.0.0.1 [::1] 51.210.159.215'
-
-export EMAIL_USE_TLS=true
-export EMAIL_HOST=smtp.gmail.com
-export EMAIL_PORT=***REMOVED***
-export EMAIL_HOST_USER=cadcoursadomicile@gmail.com
-export EMAIL_HOST_PASSWORD=azereza23.
+SECRET_KEY=***REMOVED***
+DEBUG=0
+SQL_DATABASE=***REMOVED***
+DJANGO_ALLOWED_HOSTS='cadcoursadomicile.com 127.0.0.1 [::1] 51.210.159.215'
+EMAIL_USE_TLS=true
+EMAIL_HOST=smtp.gmail.com
+EMAIL_PORT=***REMOVED***
+EMAIL_HOST_USER=cadcoursadomicile@gmail.com
+EMAIL_HOST_PASSWORD=azereza23.
