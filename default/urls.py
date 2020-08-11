e0bfed52f8a38f343425922f3be7d8ae5f9e8b54
Minigrim0 <florent.grimau@outlook.com>
2020-08-11 17:57:30 +0200

    [Upd] url routing

--- default/urls.py
+++ default/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 import default.views
 
 urlpatterns = [
-    url(r'^$', default.views.home, name="home"),
-    url(r'contact/', default.views.contactView, name="contact_view"),
+    url('^$', default.views.home, name="home"),
+    url('^contact/$', default.views.contactView, name="contact_view"),
 ]
--- inscription/urls.py
+++ inscription/urls.py
@@ -3,18 +3,11 @@ from django.conf.urls import url
 import inscription.views
 
 urlpatterns = [
-    url('confirm/',
-        inscription.views.confirmation, name="confirmation"),
-    url('payment/',
-        inscription.views.paymentView, name="paymentView"),
-    url('paylater/',
-        inscription.views.pay_later, name="pay_later"),
-    url('thanks/',
-        inscription.views.thanks, name="thanks_view"),
-    url('student/',
-        inscription.views.registerStudentView, name="registerStudent"),
-    url('coach/',
-        inscription.views.registerCoachView, name="registerCoach"),
-    url('',
-        inscription.views.registerStudentView, name="registerStudentDefault"),
+    url('^confirm/$', inscription.views.confirmation, name="confirmation"),
+    url('^payment/$', inscription.views.paymentView, name="paymentView"),
+    url('^paylater/$', inscription.views.pay_later, name="pay_later"),
+    url('^thanks/$', inscription.views.thanks, name="thanks_view"),
+    url('^student/$', inscription.views.registerStudentView, name="registerStudent"),
+    url('^coach/$', inscription.views.registerCoachView, name="registerCoach"),
+    url('^$', inscription.views.registerStudentView, name="registerStudentDefault"),
 ]
