abe9f18b094abad4049e0f6a6c356764a5540fc4
Minigrim0 <florent.grimau@outlook.com>
2020-08-11 19:28:07 +0200

    [Fix] autocomplete

--- users/autocomplete.py
+++ users/autocomplete.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
 
-        qs = User.objects.filter(profile__account_type="Coach")
+        qs = User.objects.filter(profile__account_type="coach")
 
         if self.q:
             qs = qs.filter(first_name__istartswith=self.q)
