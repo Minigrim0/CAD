d4d3814378e63abc402faa0acd4e4d02e6e7fd68
Minigrim0 <florent.grimau@outlook.com>
2020-08-17 23:27:59 +0200

    [Add] sentry SDK for error reporting

--- cad/settings.py
+++ cad/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import logging
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="***REMOVED***",
+    integrations=[DjangoIntegration()],
+
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
