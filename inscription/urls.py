7ffa1e179f9ba5f070ce345140997243b834c566
Minigrim0 <florent.grimau@outlook.com>
2020-08-19 00:48:07 +0200

    [Upd] logging + view name

--- inscription/urls.py
+++ inscription/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 import inscription.views
 
 urlpatterns = [
-    url('^confirm/$', inscription.views.confirmation, name="confirmation"),
+    url('^confirm/$', inscription.views.confirmation_view, name="confirmation"),
     url('^payment/$', inscription.views.paymentView, name="paymentView"),
     url('^paylater/$', inscription.views.pay_later, name="pay_later"),
     url('^thanks/$', inscription.views.thanks, name="thanks_view"),
