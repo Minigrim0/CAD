cd6648cbae230cdcf195bc6e8df08300a6ef6572
Minigrim0 <florent.grimau@outlook.com>
2020-08-27 10:48:57 +0200

    [Upd] logging + log compression

--- compress_logs.sh
+++ compress_logs.sh
@@ -5,5 +5,5 @@ DIRNAME="$HOME/docker/apps/logs/$(date +'%Y-%m')/"
 mkdir -p $DIRNAME
 docker cp cad_web_1:/home/app/web/logs/cad.log $DIRNAME$LOGNAME
 cd $DIRNAME
-tar cf "$DIRNAME$LOGNAME.tar" $DIRNAME$LOGNAME
+tar cf "$DIRNAME$LOGNAME.tar" $LOGNAME
 rm -f $DIRNAME$LOGNAME
