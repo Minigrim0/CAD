80924a65aef2b914645f2c9c1498a34ef2074733
Minigrim0 <florent.grimau@outlook.com>
2020-08-28 19:57:17 +0200

    [Add] workflow launch + view replacement

--- users/models.py
+++ users/models.py
@@ -111,7 +111,7 @@ class StudentAccount(models.Model):
         default=False, verbose_name="Proposition de désinscription envoyée")
     # User has payed the two first hours of course
     coach = models.ForeignKey(
-        User, null=True, blank=True, related_name="Coach", on_delete=models.SET_NULL)
+        "users.CoachAccount", null=True, blank=True, related_name="Coach", on_delete=models.SET_NULL)
     confirmedAccount = models.BooleanField(
         default=False, verbose_name="A payé ses 2 premières heures de cours")
     zip = models.CharField(
@@ -164,12 +164,14 @@ class CoachAccount(models.Model):
     nationalRegisterID = models.CharField(
         null=False, verbose_name="numéro de registre national",
         default="Inconnu", max_length=50)
-    nbStudents = models.IntegerField(
-        null=True, blank=True, verbose_name="nombre d'étudiants du coach",
-        default=0)
     confirmedAccount = models.CharField(
         default="a", blank=True, choices=coach_states, verbose_name="Etat", max_length=1)
 
+    @property
+    def nb_students(self):
+        if 'studentaccount_set' in dir(self.profile.user):
+            return self.profile.user.studentaccount_set.objects.count()
+        return 0
 
 class studentRequest(models.Model):
     """
@@ -180,10 +182,9 @@ class studentRequest(models.Model):
     # represents the user who made the request
     student = models.OneToOneField(User, on_delete=models.CASCADE)
     # represents the coaches who accepted this request
-    coaches = models.ManyToManyField(Profile)
+    coaches = models.ManyToManyField("users.CoachAccount", blank=True, related_name="request_participated")
     is_closed = models.BooleanField(default=False)
-    choosenCoach = models.CharField(
-        null=True, default='Pas encore de coach choisi', max_length=100)
+    choosenCoach = models.ForeignKey("users.CoachAccount", blank=True, null=True, on_delete=models.SET_NULL)
 
 
 class Notification(models.Model):
--- users/urls.py
+++ users/urls.py
@@ -1,22 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 from users.autocomplete import UserAutocomplete
 import users.views as views
 
 
 urlpatterns = [
-    url('^disconnect/$', views.disconnect, name="disconnect_view"),
-    url('^remove_notif/$', views.remove_notif, name="rm_notif"),
-    url('^modify_balance/$', views.modify_balance, name="modify_balance"),
-    url('^send_notif/$', views.send_notif, name="send_notif"),
-    url('^me/follow/$', views.followView, name="follow_up"),
-    url('^me/students/$', views.studentsView, name="my_students"),
-    url('^me/$', views.userView, name="my_account"),
-    url('^choose/$', views.chooseCoach),
-    url('^manage_requests/$', views.requestManage, name="manage_request"),
-    url('^requests/<int:id>/$', views.requestView),
-    url('^requests/$', views.requestView),
-    url('^error/$', views.ErrorView, name="Error_view"),
-    url('^user-autocomplete/$', UserAutocomplete.as_view(), name='user-autocomplete'),
-    url('^login$', views.login_view, name="login_view"),
-    url('^usermail$', views.get_users, name="get_users_mail"),
+    path('', views.user_home, name="user_home"),
+    path('disconnect/', views.disconnect, name="disconnect_view"),
+    path('remove_notif/', views.remove_notif, name="rm_notif"),
+    path('send_notif/', views.send_notif, name="send_notif"),
+    path('me/follow/', views.followView, name="follow_up"),
+    path('me/students/', views.studentsView, name="my_students"),
+    path('me/', views.userView, name="my_account"),
+    path('requests/', views.requestView, name="request_view"),
+    path('requests/accept/', views.acceptRequest, name="manage_request"),
+    path('user-autocomplete/', UserAutocomplete.as_view(), name='user-autocomplete'),
+    path('login/', views.login_view, name="login_view"),
+    path('usermail/', views.get_users, name="get_users_mail"),
 ]
