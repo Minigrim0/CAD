b653cc9558529d851ae506b847c668246aeca4f9
Minigrim0 <florent.grimau@outlook.com>
2020-09-13 11:11:32 +0200

    [Upd] tags, avoid showing coaches who refused

--- users/templatetags/requests_tags.py
+++ users/templatetags/requests_tags.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.simple_tag
 def coach_schedule(coach, request):
     return coach.profile.coachaccount.schedule(request)
+
+
+@register.simple_tag
+def accepted_by_coach(request, coach):
+    return request.coachrequestthrough_set.get(
+        coach=coach).has_accepted
