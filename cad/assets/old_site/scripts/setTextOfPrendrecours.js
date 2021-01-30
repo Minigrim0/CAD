81e1e93afb1f7e9868a078648208c33d1b9ea930
Minigrim0 <grimauflorent@gmail.com>
2021-01-30 23:19:32 +0100

    [Add] integrated old site into new one

---
+++ cad/assets/old_site/scripts/setTextOfPrendrecours.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#aboveIframeInscription").html(
+		''
+	);
+	$("#iframeHolderPrendreCours").html(
+		'<iframe src="https://docs.google.com/forms/d/1WlXDDVnXEt_gCU6yC7NZCfskyf2yWoGIchxc4XOKX9I/viewform" id="iframePrendre"></iframe>'
+	);
+});
