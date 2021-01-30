67a62e906549e8b74155182338f995a2e44a42f3
Minigrim0 <grimauflorent@gmail.com>
2021-01-30 23:24:56 +0100

    [Fix] Flake8 + forgot to remove comments

--- old_site/views.py
+++ old_site/views.py
@@ -5,8 +5,8 @@ from django.http import Http404
 def old(request, name="index.htm"):
     if name[-1] == "/":
         name = name[:-1]
-    #try:
-    return render(request, f"old_site/{name}")
-    #except Exception as e:
-    #    print(name, "\n\n\n\n", e)
-    #    raise Http404
+    try:
+        return render(request, f"old_site/{name}")
+    except Exception as e:
+        print(name, "\n\n\n\n", e)
+        raise Http404
