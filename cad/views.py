30fe60a7f8e234edea993612d1f234ca704c355b
Minigrim0 <grimauflorent@gmail.com>
2021-04-18 21:40:31 +0200

    [Add] password reset

--- cad/views.py
+++ cad/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from django.shortcuts import render
+
+from django.urls import reverse
 
 
 def chooseLocation(request):
     return render(request, "chooseLocation.html")
+
+
+def password_reset_done(request):
+    messages.success(
+        request,
+        "Votre mot de passe a bien été réinitialisé, vous pouvez vous connecter avec votre nouveau mot de passe"
+    )
+
+    return HttpResponseRedirect(reverse('home'))
