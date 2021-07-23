d4b4922cbbee22610dba0f21c9122a899ad5b5b4
Minigrim0 <grimauflorent@gmail.com>
2021-07-23 12:05:34 +0200

    [Fix] missing imports

--- users/views.py
+++ users/views.py
@@ -1,12 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.http import (
     HttpResponse,
-    HttpResponseBadRequest,
     HttpResponseRedirect,
-    Http404,
 )
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
