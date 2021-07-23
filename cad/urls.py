d06bd99ea5232ff4ed2b321b5f66d91d75c4cbe5
Minigrim0 <grimauflorent@gmail.com>
2021-07-23 11:43:19 +0200

    [Upd] moved api urls in cad urls

--- cad/urls.py
+++ cad/urls.py
@@ -13,18 +13,19 @@ sitemaps = {
 
 urlpatterns = [
     path("bruxelles/", include("old_site.urls")),
-    path("admin/", admin.site.urls),
+    path("namur/", include("default.urls")),
     path("administration/", include("administration.urls")),
     path("inscription/", include("inscription.urls")),
     path("users/", include("users.urls")),
+    path("admin/", admin.site.urls),
     path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
-    path("namur/", include("default.urls")),
-    path("", views.chooseLocation),
     path('auth/reset/done/', views.password_reset_done),
     path('auth/', include('django.contrib.auth.urls')),
+    path('api/', include('cad.api_urls', namespace="api")),
+    path("", views.chooseLocation),
 ]
