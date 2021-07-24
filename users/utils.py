b87c7c1cb180ce7c00e1a2a0d3dd8900ac349762
Minigrim0 <grimauflorent@gmail.com>
2021-07-24 11:29:10 +0200

    [Add] trigger element

---
+++ users/utils.py
@@ -0,0 +1,10 @@
+from users.models import CoachAccount, StudentRequest
+
+
+def findRequestsForCoach(coach_account: CoachAccount):
+    """Finds opened request fitting the coach
+
+    Args:
+        coach_account (CoachAccount): The coach for whom to find requests
+    """
+    pass
