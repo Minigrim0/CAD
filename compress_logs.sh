0a551af2c988b0111c8486806db1da4c27c88506
Minigrim0 <grimauflorent@gmail.com>
2021-07-28 21:45:04 +0200

    [Del] unused script

--- compress_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOGNAME="$(date +'%Y-%m-%d').log"
-DIRNAME="$HOME/docker/apps/logs/$(date +'%Y-%m')/"
-mkdir -p $DIRNAME
-docker cp cad_web_1:/home/app/web/logs/cad.log $DIRNAME$LOGNAME
-cd $DIRNAME
-tar cf "$DIRNAME$LOGNAME.tar" $LOGNAME
-rm -f $DIRNAME$LOGNAME
