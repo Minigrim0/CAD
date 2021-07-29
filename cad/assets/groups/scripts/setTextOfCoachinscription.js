12cf1230995d615542db09ac6de262da06a03adf
Minigrim0 <grimauflorent@gmail.com>
2021-07-29 16:05:51 +0200

    [Fix] text links

--- cad/assets/groups/scripts/setTextOfCoachinscription.js
+++ cad/assets/groups/scripts/setTextOfCoachinscription.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){	$("#aboveIframeInscription").html(		''	);	$("#iframeHolderCoach").html(		'<iframe src="https://docs.google.com/forms/d/1e3kOoPXKxsnTGjzuK0sKj9v763bFQz3mWA3l-e5OIzc/viewform?usp=send_form" id="iframeCoach" seamless="seamless"></iframe>'	);});
+$(document).ready(
+    function(){
+        $("#aboveIframeInscription").html('');
+        $("#iframeHolderCoach").html('<iframe src="https://docs.google.com/forms/d/1e3kOoPXKxsnTGjzuK0sKj9v763bFQz3mWA3l-e5OIzc/viewform?usp=send_form" id="iframeCoach" seamless="seamless"></iframe>');
+    }
+);
