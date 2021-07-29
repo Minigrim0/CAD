d3ad481fdc50ab03948612bcb4750a21269c3638
Minigrim0 <grimauflorent@gmail.com>
2021-07-29 23:22:10 +0200

    [Upd] moved docker files in fdocker folder

--- build_prod.sh
+++ build_prod.sh
@@ -1,5 +1,5 @@
 git pull
-docker-compose up -d --build
-docker-compose exec web python manage.py migrate --noinput
-docker-compose exec web python manage.py collectstatic --no-input --clear
-docker-compose restart nginx
+docker-compose -f docker/docker-compose.yml up -d --build
+docker-compose -f docker/docker-compose.yml exec web python manage.py migrate --noinput
+docker-compose -f docker/docker-compose.yml exec web python manage.py collectstatic --no-input --clear
+docker-compose -f docker/docker-compose.yml restart nginx
