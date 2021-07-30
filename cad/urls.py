6e51c063881e42ad8c8245929e31375fc6b6996e
Minigrim0 <grimauflorent@gmail.com>
2021-07-30 16:34:01 +0200

    [Fix] url to root in error pages

--- cad/urls.py
+++ cad/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
-    path("", views.chooseLocation),
+    path("", views.chooseLocation, name="root"),
 ]
 
 if settings.FULL_DEPLOY:
