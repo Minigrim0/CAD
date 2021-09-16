b0609553fca4fb70a99b56a529645c43c990108b
Minigrim0 <grimauflorent@gmail.com>
2021-09-16 14:46:44 +0200

    [Upd] groups text

--- cad/assets/groups/scripts/setTextOfIndex.js
+++ cad/assets/groups/scripts/setTextOfIndex.js
@@ -1,10 +1,20 @@
-$(document).ready(
-    function(){
-        $("#cadSubTitle").html('CAD - le premier réseau belge spécialisé en math, chimie, physique et français dès 7,80€/h');
-        $("#leftCellTitle").html('COURS EN PETITS GROUPES (dès 7,80€/h)');
-        $("#leftCellText").html('Les cours en petits groupes se donnent sous la forme d\'une étude dirigée à distance, via skype, pour le primaire et secondaire. Vous pouvez travailler avec l\'aide d\'un professeur qui vous guide dans votre étude afin de vous encadrer au mieux.<br><br>HORAIRE DES COURS <br> les samedis de 10:40 à 12:20. <br> <br>PRIX <br>-95€ pour cinq cours consécutifs <br>-125€ pour huit cours consécutifs <br><br>Inscrivez-vous en ligne en cliquant sur <u> <a href="http://www.cadcours.com/groups/prendrecours.html">prendre cours</a></u><br><br>DATES DES COURS<br> 19/9--26/9--3/10--10/10--17/10--<s>24/10--14/11</s>--21/11--28/11--5/12--12/12--9/1--16/1--23/1--30/1--6/2--27/2--6/3--13/3--20/3--27/3--24/4--8/5--15/5--22/5--29/5--5/6--12/6');
-        $("#rightCellTitle").html('COURS INDIVIDUELS À DOMICILE ou À DISTANCE - 25€/h');
-        $("#rightCellText").html('Nous organisons des cours à domicile ou à distance 365 jours par an pour encadrer vos enfants en mathématiques et sciences. Plus d\'info sur <u> <a href="http://www.cadcours.com/bruxelles">http://www.cadcours.com</a></u>');
-        $("#indexSlogan").html('Préparez-vous à la réussite !');
-    }
-);
+$(document).ready(function(){
+	$("#cadSubTitle").html(
+		'CAD - le premier réseau belge spécialisé en math, chimie, physique et français dès 7,80€/h'
+	);
+	$("#leftCellTitle").html(
+		'COURS EN PETITS GROUPES (dès 7,80€/h)'
+	);
+	$("#leftCellText").html(
+		'Les cours en petits groupes se donnent sous la forme d\'une étude dirigée pour le primaire et secondaire. Vous pouvez travailler avec l\'aide d\'un professeur qui vous guide dans votre étude afin de vous encadrer au mieux.<br><br>HORAIRE DES COURS<br>les samedis de 10:40 à 12:20.<br><br>PRIX<br>-95€ pour cinq cours consécutifs<br>-125€ pour huit cours consécutifs<br><br><br>LIEU DES COURS<br>Nous avons les antennes suivantes:<br>-Everna (Rue Saint-Vincent 30 à 1140 Evere)<br>-de Rinck (Place de la Vaillance 7 à 1070 Anderlecht)<br>-Pianofabriek (Rue du Fort 35 à 1060 St Gilles)<br>-Communa (Rue du Monténégro 144 à 1190 Forest)<br><br>Inscrivez-vous en ligne en cliquant sur <u> <a href="/groups/prendrecours.html">prendre cours</a></u><br><br>DATES DES COURS<br>25/9 - 2/10 - 9/10 - 16/10 - 23/10 - 30/10 - 13/11 - 20/11 - 27/11 - 4/12 - 11/12 - 18/12 - 15/1 - 22/1 - 29/1 - 5/2 - 12/2 - 19/2 - 26/2 - 12/3 - 19/3 - 26/3 - 2/4 - 23/4 - 30/4 - 7/5 - 14/5 - 21/5 - 28/5 - 4/6 - 11/6 - 18/6'
+	);
+	$("#rightCellTitle").html(
+		'COURS INDIVIDUELS À DOMICILE - 25€/h'
+	);
+	$("#rightCellText").html(
+		'Nous organisons des cours à domicile 365 jours par an pour encadrer vos enfants en mathématiques et sciences. Plus d\'info sur <u> <a href="http://www.cadcours.com/bruxelles/index.html">http://www.cadcours.com/bruxelles</a></u>'
+	);
+	$("#indexSlogan").html(
+		'Préparez-vous à la réussite !'
+	);
+});
