1035d8abf06628e54ed8f1d9f2fcebdabd71d686
Minigrim0 <grimauflorent@gmail.com>
2021-09-16 14:41:31 +0200

    [Upd] brussels text

--- cad/assets/old_site/scripts/setTextOfIndex.js
+++ cad/assets/old_site/scripts/setTextOfIndex.js
@@ -1,10 +1,20 @@
-$(document).ready(
-    function(){
-        $("#cadSubTitle").html('CAD - le réseau belge de soutien scolaire spécialisé en math, chimie, physique et français dès 7,80€/h - niveaux: primaire, secondaire');
-        $("#leftCellTitle").html('COURS INDIVIDUELS À DOMICILE ou À DISTANCE - 25€/h');
-        $("#leftCellText").html('COURS À DOMICILE <br>Nous organisons des cours à domicile, pour encadrer vos enfants en mathématiques et sciences. Il s\'agit de la formule la plus flexible au niveau agenda pour respecter au mieux vos contraintes horaires. Les cours sont dispensés par des étudiants universitaires sélectionnés et partageant le goût d\'une pédagogie active et concrète, basée sur l\'exemple, pour donner du sens à la matière.<br> <br> <br>COURS A DISTANCE <br>Vous pourrez partager des exercices avec le coach pour qu\'il les corrige avec l\'élève et qu\'il lui explique par video-conférence les concepts. C\'est une formule flexible au niveau horaire et efficace. <br> <br>  Nos cours sont disponibles en français et néerlandais. <br>  Après inscription, un conseiller pédagogique entrera gratuitement en communication avec vous pour répondre au mieux à votre demande. <br> ');
-        $("#rightCellTitle").html('COURS EN PETITS GROUPES (dès 7,80€/h)');
-        $("#rightCellText").html('Nous organisons une étude dirigée pour le primaire et secondaire. Vous pouvez travailler avec l\'aide d\'un professeur qui vous guide dans votre étude afin de vous encadrer au mieux.<br>ADRESSE ET HORAIRE DES COURS <br>Everna - rue St Vincent 30 à 1140 Evere <br> les samedis de 10:40 à 12:20.<br>Inscrivez-vous en ligne en cliquant sur <u> <a href="http://www.cadcours.com/groups/prendrecours.html">prendre cours</a></u>');
-        $("#indexSlogan").html('Préparez-vous à la réussite !');
-    }
-);
+$(document).ready(function(){
+	$("#cadSubTitle").html(
+		'CAD - le réseau belge de soutien scolaire spécialisé en math, chimie, physique et français dès 7,80€/h - niveaux: primaire, secondaire'
+	);
+	$("#leftCellTitle").html(
+		'COURS INDIVIDUELS À DOMICILE - 25€/h'
+	);
+	$("#leftCellText").html(
+		'Nous organisons des cours à domicile, pour encadrer vos enfants en mathématiques et sciences. Il s\'agit de la formule la plus flexible au niveau agenda pour respecter au mieux vos contraintes horaires. Les cours sont dispensés par des étudiants universitaires sélectionnés et partageant le goût d\'une pédagogie active et concrète, basée sur l\'exemple, pour donner du sens à la matière.<br> <br>Si vous désirez des cours à distance, envoyez-nous votre demande à cadcours@ymail.com'
+	);
+	$("#rightCellTitle").html(
+		'COURS EN PETITS GROUPES (dès 7,80€/h)'
+	);
+	$("#rightCellText").html(
+		'Nous organisons une étude dirigée pour le primaire et secondaire. Vous pouvez travailler avec l\'aide d\'un professeur qui vous guide dans votre étude afin de vous encadrer au mieux.<br><br>ADRESSE ET HORAIRE DES COURS <br>-Everna (Rue Saint-Vincent 30 à 1140 Evere)<br>-de Rinck (Place de la Vaillance 7 à 1070 Anderlecht)<br>-Pianofabriek (Rue du Fort 35 à 1060 St Gilles)<br>-Communa (Rue du Monténégro 144 à 1190 Forest)<br><br>HORAIREles samedis de 10:40 à 12:20.<br>Inscrivez-vous en ligne en cliquant sur <u> <a href="/bruxelles/prendrecours.html">prendre cours</a></u>'
+	);
+	$("#indexSlogan").html(
+		'Préparez-vous à la réussite !'
+	);
+});
